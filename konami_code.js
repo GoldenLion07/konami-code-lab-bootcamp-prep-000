45e8e0bc0356e68c18d5e1a5fb8f79ab57d31df0
gmat191@gmail.com <gmat191@gmail.com>
2018-06-14 23:11:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -30,6 +30,7 @@ function onKeyDownHandler(e) {
 }
 
 function init() {
+  
   var body = document.body;
 +  body.addEventListener('keydown', function(e){
 +    var key = parseInt(e.detail || e.which);
