aa1cb2badcc557f0c1bed2b67f43172d959bf8f8
gmat191@gmail.com <gmat191@gmail.com>
2018-06-14 23:01:03 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -32,34 +32,4 @@ function onKeyDownHandler(e) {
 function init() {
   // your code here
  var body = document.body;
-  body.addEventListener('keydown', function(e){
-    var key = parseInt(e.detail || e.which);
-
-    // check if key is equal to current value of index in code array
-    // then loop through the array if conditions match
-
-    if (key === codes[index]) {
-      // if yes, increment index by 1
-      index++;
-
-      // if still in the loop, check if index is equal to code array length
-      if (index === codes.length - 1) {
-        // reset the index counter
-        index = 0;
-        // if yes, you passed, congrats!
-        alert("Congrats!");
-      }
-
-
-
-  // if key doesn't match current index in code array, then reset the index to zero
-  } else {
-    index = 0;
-  }
-
-
-  });
-
 }
-
-init();
