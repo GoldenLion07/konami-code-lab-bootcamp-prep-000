9ee228187a40a7d787a70178f4ae3c7e70328dbc
gmat191@gmail.com <gmat191@gmail.com>
2018-06-14 23:07:53 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -34,5 +34,16 @@ function init() {
  var body = document.body;
   body.addEventListener('keydown', function(e){
     var key = parseInt(e.detail || e.which);
+if (key === code[index]) {
+      index++;
+if (index === code.length - 1) {
+        index = 0;
+        alert("Congrats!");
+      }
+} else {
+    index = 0;
   }
+});
 }
+
+init();
